44ccdceed331935c2347bfdf0083be9790163699
gksktnsdl <gksktnsdl@naver.com>
2014-10-02 12:24:32 +0900

    d

--- sketch.js
+++ sketch.js
@@ -1,52 +1,34 @@
+var trailX = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var trailY = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 function setup()
 {
-	createCanvas(800,600);
-	noFill();
-	stroke(255,0,0);
-
+     createCanvas(800, 600);
+    
+     noFill();
+     stroke(255, 0, 0);
+    
 }
 
 function draw()
 {
-	background(0);
-	ellipse(mouseX, mouseY, 50, 50);
-	var trailX = 0;
-    var trailY = 0;
+     background(0);
 
-    trailX = (mouseX + trailX) / 2;
-    trailY = (mouseY + trailY) / 2;
-     
-    trailX = (mouseX + trailX * 9) / 10;
+     // mouse ellipse
+     ellipse(mouseX, mouseY, 100, 100);
 
-    var trailX0 = 0;
-    var trailX1 = 0;
+     // trail ellipse #0
+     trailX[0] = (mouseX + trailX[0] * 4) / 5;
+     trailY[0] = (mouseY + trailY[0] * 4) / 5;
+     ellipse(trailX[0], trailY[0], 80, 80);
 
-    trailX0 = (mouseX + trailX0) / 2;
-    trailX1 = (trailX0 + trailX1) / 2;
- 
-    var trailX0 = 0;
-    var trailX1 = 0;
-
-    trailX0 = (mouseX + trailX0) / 2;
-    var trailX0 = 0;
-    var trailX1 = 0;
-    var trailX2 = 0;
-    var trailX3 = 0;
-
-    trailX0 = (mouseX + trailX0) / 2;
-    trailX1 = (trailX0 + trailX1) / 2;
-    trailX2 = (trailX1 + trailX2) / 2;
-    trailX3 = (trailX2 + trailX3) / 2;
-
-    var trailX = [0, 0, 0, 0];
-
-    trailX[0] = (mouseX + trailX[0]) / 2;
-    trailX[1] = (trailX[0] + trailX[1]) / 2;
-    trailX[2] = (trailX[1] + trailX[2]) / 2;
-    trailX[3] = (trailX[2] + trailX[3]) / 2;
-
-    for (var i = 1; i < 4; i = i + 1)
+     for (var i = 1; i < 10; i = i + 1)
      {
-       trailX[i] = (trailX[i-1] + trail[i]) / 2;
+     trailX[i] = (trailX[i-1] + trailX[i] * 4) / 5;
+     trailY[i] = (trailY[i-1] + trailY[i] * 4) / 5;
+     ellipse(trailX[i], trailY[i], 80, 80);
      }
+
 }
+
+
